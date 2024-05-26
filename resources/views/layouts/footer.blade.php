f89dabf48459b663e4011794bc0c053f0556ea6c
Gian Carlo De Vero <0giancarlo0@gmail.com>
2024-05-26 21:53:24 +0800

    Nice Admin Sucessfully Intergrated!

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Laravel Gian</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Modified by <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fm.me%2Fgiandv.1006%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR38YK9VxHM8Wb9W3KTFy9_m4W_Ah422RgZEq99UOmwB-DUmeyeQkJg58xg_aem_Ac_H-L2Reuq5Uxq5P2R1ToFDPX7tGKoPKzsxVwuao8zfUY_yBiBzTpNqLTT5rfeNE_Cx8RM_rwiImA7CiYu3yY7U&h=AT3QKPqpLM7uqRq5X_IU9vDT33ae3mGnW88IuJDnCrQ-pK09RPkpLJMea6LFEpDiJucpqdi-JaTMn9zha3k2MiQpzP9TTpJSOF3pqROoEbPi3Ff0YzymBBL8u7jonw4VzidQvg">Gian Carlo De Vero</a>
+    </div>
+</footer>
